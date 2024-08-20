4ecc1ee4cdde53dcd156bb4a8d4a7d2657e9ea8d
Timothy Adewuyi <adewuyitimothy10@gmail.com>
2024-08-20 08:40:43 +0100

    Initial Commit

---
+++ cuddleme.mobile_swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cuddleme.mobile_swift
+//
+//  Created by Gideon Adewuyi on 20/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cuddleme.mobile_swift/cuddleme_mobile_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  cuddleme_mobile_swiftApp.swift
+//  cuddleme.mobile_swift
+//
+//  Created by Gideon Adewuyi on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct cuddleme_mobile_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
